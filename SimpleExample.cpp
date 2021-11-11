d15d47955af549d1b026ce872791ea6607539317
brend3n <brenden@knights.ucf.edu>
2021-11-11 00:00:36 -0500

    Added code for moving motor with joystick

--- SimpleExample.cpp
+++ SimpleExample.cpp
@@ -12,9 +12,42 @@
 #define dirPinx2  -1
 #define stepPinx2 -1
 
+#define dirPinz  -1
+#define stepPinz -1
+
+#define dirPiny  -1
+#define stepPiny -1
+
+// if one stepper for gantry
+// #define dirPinGantry -1
+// #define stepPinGantry -1
+
+
+#define x_dir A0
+#define y_dir A1
+#define switch_s 13
+
+int state = 1;
+int prev_state = 0;
+
+int x_pos;
+int y_pos;
+int mapX;
+int mapY;
+
+bool gantryIsSet = false;
+
 // Instantiating motor driver objects
 AccelStepper motor_x1 = AccelStepper(interface, stepPinx1, dirPinx1);
 AccelStepper motor_x2 = AccelStepper(interface, stepPinx2, dirPinx2);
+AccelStepper motor_y = AccelStepper(interface, stepPiny, dirPiny);
+AccelStepper motor_z = AccelStepper(interface, stepPinz, dirPinz);
+
+// if one stepper for gantry
+// AccelStepper gantry = AccelStepper(interface, stepPinGantry, dirPinGantry);
+
+AccelStepper* current_motor;
+
 
 MultiStepper gantry;
 
@@ -46,26 +79,98 @@ void test_run_group(){
     // Same as before but other direction
     positions[0] = (-1*steps);
     positions[1] = steps;
-    
+
     gantry.moveTo(positions);
     gantry.runSpeedToPosition();
     delay(1010);
 }
 void setup(){
+
+    Serial.begin(9600);
+
+  pinMode(x_dir, INPUT);
+  pinMode(y_dir, INPUT);
+  pinMode(switch_s, INPUT_PULLUP);
+
     // Set maxmium speeds
     motor_x1.setMaxSpeed(MAX_SPEED);
     motor_x2.setMaxSpeed(MAX_SPEED);
+    motor_y.setMaxSpeed(MAX_SPEED);
+    motor_z.setMaxSpeed(MAX_SPEED);
 
     // Set acceleration
     motor_x1.setAcceleration(MAX_ACCELERATION);
     motor_x2.setAcceleration(MAX_ACCELERATION);
+    motor_y.setAcceleration(MAX_ACCELERATION);
+    motor_z.setAcceleration(MAX_ACCELERATION);
 
     // Add motors to multistepper object
     gantry.addStepper(motor_x1);
     gantry.addStepper(motor_x2);
 
 }
+void control_motor(){
+    x_pos = analogRead(x_dir);
+    y_pos = analogRead(y_dir);
+
+    mapX = map(x_pos, 0,1024,-512,512);
+    mapY = map(y_pos, 0,1024,-512,512);
+
+    if (state != prev_state){
+        if(state == 1){
+            Serial.println("Controlling: Gantry ");
+            current_motor = &motor_x;
+            // current_motor = &gantry;
+        }else if(state == 2){
+            Serial.println("Controlling: Y ");
+            current_motor = &motor_y;
+        }else if{
+            Serial.println("Controlling: Z ");
+            gantryIsSet = true;
+        }else{
+            gantryIsSet = false;
+        }
+    }
+    if(mapX < 0 || mapY < 0){
+        Serial.println("LEFT or DOWN");
+        if (gantryIsSet){
+            long pos;
+            pos[0] =  1;
+            pos[1] = -1;
+            gantry.moveTo(pos);
+            gantry.runSpeedToPosition();
+        }else{
+            *current_motor.moveTo(-10);
+            *current_motor.runSpeedToPosition();
+        }
+    }else if(mapX > 0 || mapY > 0){
+        Serial.println("RIGHT or UP");
+        if (gantryIsSet){
+            long pos;
+            pos[0] = -1;
+            pos[1] =  1;
+            gantry.moveTo(pos);
+            gantry.runSpeedToPosition();
+        }else{
+            *current_motor.moveTo(10);
+            *current_motor.runSpeedToPosition();
+
+        }
+    }
+
+    if(digitalRead(switch_s) == HIGH){
+        if (state == 3){
+            prev_state = 3;
+            state = 1;
+        }else{
+            prev_state = state;
+            state++;
+        }
+    }
+}
 
 void loop(){
-    test_run_group();
+    // test_run_group();
+    control_motor();
+
 }
