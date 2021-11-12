f57cd7af6892a6e197dced12e98101d20f73245b
brend3n <brenden@knights.ucf.edu>
2021-11-11 19:22:20 -0500

    Simplified code

--- SimpleExample.ino
+++ SimpleExample.ino
@@ -1,25 +1,26 @@
 // Motors
 #include <AccelStepper.h>
 #include <MultiStepper.h>
+#include <Servo.h>
 
 // Motor driver type
 #define interface  1
 
 // Direction and Step pins for all motors
-#define dirPinx1  -1
-#define stepPinx1 -1
+#define dirPinx1  45
+#define stepPinx1 44
 
-#define dirPinx2  -1
-#define stepPinx2 -1
+#define dirPinx2  33
+#define stepPinx2 32
 
-#define dirPinz  -1
-#define stepPinz -1
+#define dirPinz  48
+#define stepPinz 49
 
 #define dirPiny  -1
 #define stepPiny -1
 
-#define CLOSE 0
-#define OPEN  180
+#define CLOSE 50
+#define OPEN  0
 
 // if one stepper for gantry
 // #define dirPinGantry -1
@@ -29,16 +30,16 @@
 #define x_dir A0
 #define y_dir A1
 #define switch_s 13
-#define buttonPin 
+#define buttonPin 12 
 
 // Switch variables
-int state = 1;
-int prev_state = 0;
+bool isOpen = false;
+int prev_state = 1;
 int switch_state;
 
 // Button variables
 int button_state;
-int button_count = 0;
+int button_count = 1;
 
 
 int x_pos;
@@ -58,6 +59,7 @@ AccelStepper motor_x1 = AccelStepper(interface, stepPinx1, dirPinx1);
 AccelStepper motor_x2 = AccelStepper(interface, stepPinx2, dirPinx2);
 AccelStepper motor_y = AccelStepper(interface, stepPiny, dirPiny);
 AccelStepper motor_z = AccelStepper(interface, stepPinz, dirPinz);
+Servo servo = Servo();
 
 // if one stepper for gantry
 // AccelStepper gantry = AccelStepper(interface, stepPinGantry, dirPinGantry);
@@ -118,7 +120,19 @@ void gripper(int a, Servo x)
       delay(15);
     }
   }
+}
+#define UP 500
+#define DOWN -500
+void test(){
+  motor_z.move(UP);
+  motor_z.runToPosition();
+  // gripper(CLOSE, servo);
 
+  // motor_z.move(150);
+  // motor_z.runToPosition();
+  // gripper(CLOSE, servo);
+
+}
 void setup(){
 
     Serial.begin(9600);
@@ -127,6 +141,8 @@ void setup(){
     pinMode(y_dir, INPUT);
     pinMode(switch_s, INPUT_PULLUP);
     pinMode(buttonPin, INPUT);
+
+    servo.attach(9);
 
     // Set maxmium speeds
     motor_x1.setMaxSpeed(MAX_SPEED);
@@ -144,8 +160,14 @@ void setup(){
     gantry.addStepper(motor_x1);
     gantry.addStepper(motor_x2);
 
-    current_motor = &motor_y;
+    servo.write(0);
+    // gripper(CLOSE, servo);
+    // current_motor = &motor_y;
+    // delay(3000);
+    // test();
 }
+
+
 void control_motor(){
     x_pos = analogRead(x_dir);
     y_pos = analogRead(y_dir);
@@ -154,12 +176,86 @@ void control_motor(){
 
     mapX = map(x_pos, 0,1024,-512,512);
     mapY = map(y_pos, 0,1024,-512,512);
+    
+    if (mapX > 400){
+      Serial.println("RIGHT");
+      // pos[0] =  10;
+      // pos[1] = -10;
+      // gantry.moveTo(pos);
+      // gantry.runSpeedToPosition();
+      motor_x1.move(10);
+      motor_x2.move(-10);
+      motor_x1.runSpeedToPosition();
+      motor_x2.runSpeedToPosition();
+    }else if (mapX < -400){
+      Serial.println("LEFT");
+      // pos[0] =  -10;
+      // pos[1] =   10;
+      // gantry.moveTo(pos);
+      // gantry.runSpeedToPosition();
+      motor_x1.move(-10);
+      motor_x2.move(10);
+      motor_x1.runSpeedToPosition();
+      motor_x2.runSpeedToPosition();
+    }else if(mapY > 400){
+      Serial.println("DOWN");
+      motor_z.move(DOWN);
+      motor_z.runToPosition();
+    }else if(mapY < -400){
+      Serial.println("UP");
+      motor_z.move(UP);
+      motor_z.runToPosition();
+    }else{
+      motor_z.stop();
+      motor_x1.stop();
+      motor_x2.stop();
 
+    }
+
+    if (switch_state == LOW){
+        button_count++;
+    }
+
+    if((button_count % 2) == 0){
+        gripper(CLOSE, servo);
+        // Serial.println("CLOSE GRIPPER");
+        // delay(5000);
+    }else{
+        gripper(OPEN, servo);
+        // Serial.println("OPEN GRIPPER");
+        // delay(5000);
+    }
+
+
+
+    // mapX = x_pos;
+    // mapY = y_pos;
+    
+
+    // Determine current motor to operate
+    // if (state != prev_state){
+        // if(state == 1){
+        //     Serial.println("Controlling: Gantry ");
+        //       gantryIsSet = true;
+        //     // current_motor = &gantry;
+        // }else if(state == 2){
+        //     Serial.println("Controlling: Y ");
+        //     current_motor = &motor_y;
+        //     gantryIsSet =false;
+        // }else if(state == 3){
+        //     Serial.println("Controlling: Z ");
+        //     current_motor = &motor_z;
+        //     gantryIsSet = false;
+        // }else{
+        //     gantryIsSet = false;
+        // }
+    // }
+    /*
     // Determine current motor to operate
     if (state != prev_state){
         if(state == 1){
             Serial.println("Controlling: Gantry ");
-              gantryIsSet = true;
+            gantryIsSet = true;
             // current_motor = &gantry;
         }else if(state == 2){
             Serial.println("Controlling: Y ");
@@ -174,7 +270,6 @@ void control_motor(){
         }
     }
 
-
     if (button_state == HIGH){
         button_count++;
     }
@@ -186,7 +281,7 @@ void control_motor(){
     }
     
     // Move the motor based on joystick position
-    if(mapX < 0 || mapY < 0){
+    if((mapX < 0 && mapY < 0) || (mapX < 0 && mapY > 0)){
         Serial.println("LEFT or DOWN");
         if (gantryIsSet){
             
@@ -198,7 +293,7 @@ void control_motor(){
             (*current_motor).move(-10);
             (*current_motor).runSpeedToPosition();
         }
-    }else if(mapX > 0 || mapY > 0){
+    }else if(((mapX > 0 && mapY > 0) || (mapX > 0 && mapY < 0))){
         Serial.println("RIGHT or UP");
         if (gantryIsSet){
             pos[0] = -10;
@@ -212,7 +307,7 @@ void control_motor(){
     }
 
     // Update last state of the switch
-    if(switch_state == HIGH){
+    if(switch_state == LOW){
         if (state == 3){
             prev_state = 3;
             state = 1;
@@ -221,10 +316,12 @@ void control_motor(){
             state++;
         }
     }
+    */
 }
+
 
 void loop(){
     // test_run_group();
     control_motor();
-
+    // test();
 }
